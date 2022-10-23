c1bcd8a56f7099a3513916fa62236b0a720a1021
itssegit <it.sse.pub@gmail.com>
2022-10-23 22:36:03 +0900

    jwt token and password encoder

--- src/pages/user/Login.js
+++ src/pages/user/Login.js
@@ -1,6 +1,22 @@
 import { Box, Stack, Button, TextField } from "@mui/material";
+import { useCallback } from "react";
+import useInput from "../../hooks/useInput";
+import AxiosModule from "../../services/itsse.axios";
 
 const Login = () => {
+    //
+    const [email, onChangeEmail] = useInput('');
+    const [password, onChangePassword] = useInput('');
+    const axiosModule = AxiosModule();
+
+    const login = useCallback(() => {
+        //
+        const loginData = {
+            email,
+            password
+        }
+        axiosModule.sendPost('http://localhost:8080/api/user/signin', loginData);
+    }, [email, password]);
     //
     return (
         <div style={{marginTop:'4rem'}}>
@@ -13,6 +29,8 @@ const Login = () => {
                         required
                         id="outlined-required"
                         label="e-메일 주소(ID)"
+                        value={email}
+                        onChange={onChangeEmail}
                         style={{width:'500px'}}
                     />
                 </div>
@@ -21,13 +39,15 @@ const Login = () => {
                         id="outlined-password-input"
                         label="비밀번호"
                         type="password"
+                        value={password}
+                        onChange={onChangePassword}
                         autoComplete="current-password"
                         style={{width:'500px', marginTop:'1.5rem'}}
                     />
                 </div>
                 <div style={{width:'500px', marginTop:'1.5rem'}}>
                     <Stack direction="row" spacing={1}>
-                        <Button variant="contained">로그인</Button>
+                        <Button variant="contained" onClick={login}>로그인</Button>
                         <Button variant="contained" color="success">회원가입</Button>
                         <Button variant="contained" color="error">비밀번호 분실</Button>
                     </Stack>
